cd12a1f9935da593cc6696e41e502fccc0867b27
Hamza156 <hamzapics09@gmail.com>
2018-10-22 15:01:36 +0500

    Hamza156

--- hello.php
+++ hello.php
@@ -1,3 +1,3 @@
 <?php 
-echo 'Hello World';
+echo 'Hello Hactober! Its an amazing opperunity for all of us to win free shirts';
 ?>
--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,1 @@
-print("Hello World")
+print("Hello Hecktober!! Its an amazing opperunnity please avail all of us!")
